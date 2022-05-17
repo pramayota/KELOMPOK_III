5517773ad45292967a73beeaebf2f1450b3d686e
John Doe <ardhiyashita2002@gmail.com>
2022-05-17 10:02:55 +0800

    update quiz

---
+++ app/src/main/java/com/prama/pahlawannasional/kuis/KuisActivity.java
@@ -0,0 +1,14 @@
+package com.prama.pahlawannasional.kuis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.prama.pahlawannasional.R;
+import android.os.Bundle;
+
+public class KuisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuis);
+    }
+}
